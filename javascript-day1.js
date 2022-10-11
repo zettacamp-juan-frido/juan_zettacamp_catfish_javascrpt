eb39939bd1ca4dbfdc25862a8b7fedbc6060a173
Juan Frido Aditiya <114280850+zettacamp-juan-frido@users.noreply.github.com>
2022-10-11 12:34:56 +0700

    change number amount discount

--- javascript-day1.js
+++ javascript-day1.js
@@ -1,8 +1,8 @@
 let book = 60000;
-let amDiscount = 5.0;
+let amDiscount = 0.5;
 let amTax = 2000;
 
-const priceDiscount = book / amDiscount;
+const priceDiscount = book * amDiscount;
 const priceTax = priceDiscount - amTax;
 console.log(priceDiscount);
 console.log(priceTax);
