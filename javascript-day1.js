e155a6f4cbee0b5815860a623d82e0cb9b1fb532
Juan Frido Aditiya <114280850+zettacamp-juan-frido@users.noreply.github.com>
2022-10-11 12:38:08 +0700

    change tax number

--- javascript-day1.js
+++ javascript-day1.js
@@ -3,6 +3,6 @@ let amDiscount = 0.5;
 let amTax = 2000;
 
 const priceDiscount = book * amDiscount;
-const priceTax = priceDiscount - amTax;
+const priceTax = priceDiscount + amTax;
 console.log(priceDiscount);
 console.log(priceTax);
