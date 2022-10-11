7b7bc2ab49e247e18e2c9667ff15cc3d71ae924b
Juan Frido Aditiya <114280850+zettacamp-juan-frido@users.noreply.github.com>
2022-10-11 22:33:32 +0700

    Finale

--- javascript-day2.js
+++ javascript-day2.js
@@ -1,30 +1,30 @@
-let book = 60000;
-let amDiscount = 0.5;
-let amTax = 2000;
-let stock = 6;
+// let book = 60000;
+// let amDiscount = 0.5;
+// let amTax = 2000;
+// let stock = 6;
 
-let priceBook = 0;
-let discount = 0;
-let total = 0;
+// let priceBook = 0;
+// let discount = 0;
+// let total = 0;
 // let bookStock = 10;
 
 // const stock = [bookStock];
 
 // const stock = 8;
-const buy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const buy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (let i = 0; i <= buy.length; i++) {
-  if (i < stock) {
-    console.log(buy[i]);
-    priceBook += book;
-    discount = priceBook * amDiscount;
-    total = discount + amTax;
-    console.log(total);
-  } else {
-    const alert = "Out off stock";
-    console.log(alert);
-  }
-}
+// for (let i = 0; i <= buy.length; i++) {
+//   if (i < stock) {
+//     console.log(buy[i]);
+//     priceBook += book;
+//     discount = priceBook * amDiscount;
+//     total = discount + amTax;
+//     console.log(total);
+//   } else {
+//     const alert = "Out off stock";
+//     console.log(alert);
+//   }
+// }
 
 // if (buy <= bookStock) {
 //   for (let i = 0; i <= stock.length; i++) {
@@ -38,7 +38,7 @@ for (let i = 0; i <= buy.length; i++) {
 //   console.log(alert);
 // }
 
-// function price() {
+// function price(buying) {
 //   for (let i = 0; i <= buy.length; i++) {
 //     if (i < stock) {
 //       console.log(buy[i]);
@@ -56,3 +56,54 @@ for (let i = 0; i <= buy.length; i++) {
 // }
 // const totalPrice = price();
 // console.log(totalPrice);
+
+const listOfBook = ["Buku A", "Buku B", "Buku C"];
+const stock = [10, 5, 9];
+
+// function buyBooks(bookName, totalBuy) {
+//   for (i = 0; i <= listOfBook.length; i ++) {
+//     console.log(listOfBook[i]); // i = 3 ; Buku C
+//     console.log(i);
+//     if (listOfBook[i] == bookName) {
+//       let currentStock = stock[i];
+//       console.log(currentStock); // i = 3 ; 9
+//       if (totalBuy <= currentStock) {
+//         const price = 5000;
+//         let total;
+//         total = totalBuy * price;
+//         return total;
+//       } else {
+//         return "Out of stock";
+//       }
+//     } else {
+//       return "Book not found";
+//     }
+//   }
+// }
+
+// let buy = buyBooks("Buku C", 9);
+// console.log(buy);
+
+function buyBooks(bookName, totalBuy) {
+  for (let i = 0; i < listOfBook.length; i++) {
+    if (bookName == listOfBook[i]) {
+      console.log(bookName); // log name book
+      let currentStock = stock[i];
+      console.log(currentStock); // log stock book
+      if (totalBuy <= currentStock) {
+        console.log(totalBuy); // log total buy
+        const price = 5000;
+        let total;
+        total = totalBuy * price;
+        return total;
+      } else {
+        return "Out of Stock";
+      }
+    } else {
+      return "Book not found";
+    }
+  }
+}
+
+let buy = buyBooks("Buku Z", 11);
+console.log(buy);
