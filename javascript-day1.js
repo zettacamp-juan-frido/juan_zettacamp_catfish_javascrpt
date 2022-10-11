9575e6556351e3a7e149469303d2b97d31bbc723
Juan Frido Aditiya <114280850+zettacamp-juan-frido@users.noreply.github.com>
2022-10-11 14:21:34 +0700

    adding function parameter

--- javascript-day1.js
+++ javascript-day1.js
@@ -2,7 +2,22 @@ let book = 60000;
 let amDiscount = 0.5;
 let amTax = 2000;
 
-const priceDiscount = book * amDiscount;
-const priceTax = priceDiscount + amTax;
-console.log(priceDiscount);
-console.log(priceTax);
+// const priceDiscount = book * amDiscount;
+// const priceTax = priceDiscount + amTax;
+// console.log(priceDiscount);
+// console.log(priceTax);
+
+function Discount() {
+  const priceDiscount = book * amDiscount;
+  return priceDiscount;
+}
+
+let priceAfter = Discount();
+console.log(priceAfter);
+
+function Tax() {
+  const priceTax = priceAfter + amTax;
+  return priceTax;
+}
+let total = Tax();
+console.log(total);
