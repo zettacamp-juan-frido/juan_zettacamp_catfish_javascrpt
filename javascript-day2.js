4b6a0b1f1733da25b034dc747f9a74b74d5c61e8
Juan Frido Aditiya <114280850+zettacamp-juan-frido@users.noreply.github.com>
2022-10-11 16:14:26 +0700

    change the variable and try to make function

--- javascript-day2.js
+++ javascript-day2.js
@@ -1,42 +1,56 @@
 let book = 60000;
 let amDiscount = 0.5;
 let amTax = 2000;
-let buy = 10;
+let stock = 6;
 
-let price = 0;
+let priceBook = 0;
 let discount = 0;
 let total = 0;
-let bookStock = 10;
+// let bookStock = 10;
 
-const stock = [bookStock];
+// const stock = [bookStock];
+
 // const stock = 8;
+const buy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// for (let i = 0; i <= stock.length; i++) {
-//   if (i < buy) {
+for (let i = 0; i <= buy.length; i++) {
+  if (i < stock) {
+    console.log(buy[i]);
+    priceBook += book;
+    discount = priceBook * amDiscount;
+    total = discount + amTax;
+    console.log(total);
+  } else {
+    const alert = "Out off stock";
+    console.log(alert);
+  }
+}
+
+// if (buy <= bookStock) {
+//   for (let i = 0; i <= stock.length; i++) {
 //     price = book * buy;
-//     discount = price / amDiscount;
-//     total = discount - amTax;
+//     discount = price * amDiscount;
+//     total = discount + amTax;
 //   }
-// }
-// if (condition) {
+//   console.log(total);
 // } else {
 //   const alert = "Out off stock";
 //   console.log(alert);
 // }
 
-if (buy <= bookStock) {
-  for (let i = 0; i <= stock.length; i++) {
-    price = book * buy;
-    discount = price * amDiscount;
-    total = discount + amTax;
-  }
-  console.log(total);
-} else {
-  const alert = "Out off stock";
-  console.log(alert);
-}
-
-// const priceDiscount = book / amDiscount;
-// const priceTax = priceDiscount - amTax;
-// console.log(priceDiscount);
-// console.log(priceTax);
+// function price() {
+//   for (let i = 0; i <= stock.length; i++) {
+//     if (i < buy) {
+//       console.log(stock[i]);
+//       priceBook += book;
+//       discount = priceBook * amDiscount;
+//       total = discount + amTax;
+//       return total;
+//     } else {
+//       const alert = "Out off stock";
+//       return alert;
+//     }
+//   }
+// }
+// const totalPrice = price();
+// console.log(totalPrice);
